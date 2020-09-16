e5f0c811cac584e7423cb6e7aaddf7ba7a657c8c
David Rivera <David.Rivera@ucop.edu>
2020-09-15 22:33:05 -0700

    update main

--- variables.tf
+++ variables.tf
@@ -2,3 +2,18 @@ variable "aws_region" {
   description = "The AWS region to create things in."
   default     = "us-west-2"
 }
+variable "name" {
+  default     = null
+  description = "Resource name"
+  type        = string
+}
+variable "tags" {
+  default     = {}
+  description = "A map of tags to add to all resources"
+  type        = map(string)
+}
+variable "enabled" {
+  default     = true
+  description = "Set to `false` to prevent the module from creating resources"
+  type        = bool
+}
